fc8b972a1faa33c6372e66592a61d789de148f45
danah olivetree <danah.olivetree@gmail.com>
2017-09-26 11:34:29 -0600

    get request in movies works

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const knex = require('knex')
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var movie = require('./routes/movie');
+var director = require('./routes/director');
 
 var app = express();
 
